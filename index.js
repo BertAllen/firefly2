e9eeb2bc8833f17e43095d5744ecd57f4c5bd5ef
Bert Allen <mrlaughingdragon@aol.com>
2016-08-10 16:39:56 -0600

    server talking ... did it myself

--- index.js
+++ index.js
@@ -4,11 +4,14 @@
     const server = express();
     const bp = require("body-parser");
     const port = 8888;
+    const events = require("events");
 
     server.listen(port, function () {
         console.log("The amazing server on port# ", port);
     });
-
-    server.get
+// server.use("/", express.static("/"))
+    server.get("/", function (req, res) {
+        res.send("all is well");
+    })
 
 }());
